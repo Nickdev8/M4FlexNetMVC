adce3fc86dcbd7146fa7c0875737b27551e08891
Nick Esselman <nick.esselman@gmail.com>
2025-05-13 12:03:31 +0200

    Add variable declarations and output statements for demonstration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 int a = 5;
+string b = "Hello";
+bool c = true;
+float d = 5.5f;
+
+
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
