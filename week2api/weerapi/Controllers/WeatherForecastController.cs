896617607c217c811bb7f6d45140196c287b91f9
Nick Esselman <nick.esselman@gmail.com>
2025-05-20 12:04:00 +0200

    city = cirty

--- week2api/weerapi/Controllers/WeatherForecastController.cs
+++ week2api/weerapi/Controllers/WeatherForecastController.cs
@@ -20,14 +20,14 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public IEnumerable<WeatherForecast> Get([FromQuery] string city)
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-            City = "amsterdam"
+            City = city
         })
         .ToArray();
     }
