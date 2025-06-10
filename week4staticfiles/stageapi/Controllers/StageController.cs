6660c9b1c0a237b790605bf78a5fe7cc709bfd2b
Nick Esselman <nick.esselman@gmail.com>
2025-06-10 12:24:12 +0200

    04 01

---
+++ week4staticfiles/stageapi/Controllers/StageController.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using stageapi.Models;
+
+namespace stageapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StageController : ControllerBase
+{
+    private readonly ILogger<StageController> _logger;
+
+    List<Stage> stages = new List<Stage>
+    {
+        new Stage { BedrijfsNaam = "Tech Innovators BV", StageVergoeding = 500.00, Stad = "Amsterdam" },
+        new Stage { BedrijfsNaam = "Green Solutions", StageVergoeding = 600.50, Stad = "Rotterdam" },
+        new Stage { BedrijfsNaam = "Future Designs", StageVergoeding = 450.75, Stad = "Utrecht" },
+        new Stage { BedrijfsNaam = "EduWorld", StageVergoeding = 400.00, Stad = "Den Haag" },
+        new Stage { BedrijfsNaam = "Code Creators", StageVergoeding = 550.00, Stad = "Eindhoven" }
+    };
+
+    public StageController(ILogger<StageController> logger)
+    {
+        _logger = logger;
+    }
+}
---
+++ week4staticfiles/stageapi/Models/Stage.cs
@@ -0,0 +1,9 @@
+namespace stageapi.Models;
+
+
+public class Stage
+{
+    public string? BedrijfsNaam { get; set; }
+    public double? StageVergoeding { get; set; }
+    public string? Stad { get; set; }
+}
